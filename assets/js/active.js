347d32a0ad9f48e4d84098766503fbd472fe0d09
rupalikar <rupalikar122@gmail.com>
2019-02-14 22:53:03 +0600

    first commit

---
+++ assets/js/active.js
@@ -0,0 +1,18 @@
+$('.active_image').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+	autoplay:true,
+	autoplayTimeout:3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
